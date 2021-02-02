9a329c880ea710272a249648c6e8281ea31812e6
unknown <13429744928@139.com>
2021-02-02 14:41:09 +0800

    添加login页、去除原本默认页特效等

---
+++ src/views/Login/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+         <el-button>默认按钮</el-button>
+  <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+        <h1>
+            你好
+            </h1>
+        hello world
+    </div>
+</template>
+<style scoped>
+
+</style>
+<script>
+export default {
+    
+}
+</script>
